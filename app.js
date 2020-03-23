c113eaace99936c005c7f13d560f548beed2e0b7
Alan R Tami <alanrobertotami@gmail.com>
2020-03-23 14:11:55 +0100

    battler no js yet

---
+++ app.js
@@ -0,0 +1,27 @@
+﻿// This function is called when the page is being loaded
+window.addEventListener('load', ()=> {
+// Everthing inside this function runs as the page is loaded.
+
+    // In order to change elements on a html document 
+    // it's handy to declare those elements as variables! 
+
+    // To reference elements we can call them by their ID (single) or Class (array) name.
+    // We do this with the following function: 
+
+    let playerTypeIcon = document.getElementById("player-type-icon"); 
+    // We first declared a variable. Then we called the function "getElementByID"
+    // on the document that this script runs on.
+    // We then specify the ID using a string. You can find which ID or Class you need by
+    // looking it up in the html document. See if you can find the "player-type-icon" ID on
+    // the battle.html document! 
+
+
+
+});
+
+
+
+ranged_attack.addEventListener("click", () =>{
+//Everything in this function runs when this button is clicked 
+  
+});
